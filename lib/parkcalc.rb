094156af54faaa235dd817e5e72b31bb479238da
Brent Johnson <bjohnson@monsooncommerce.com>
2014-08-06 10:47:57 -0700

    Valet Parking Scenarios
    Signed-off-by: Brent Johnson <bjohnson@monsooncommerce.com>

--- lib/parkcalc.rb
+++ lib/parkcalc.rb
@@ -4,10 +4,19 @@ class ParkCalcPage
 	@@leavingPrefix = 'Leaving'
 	@@dateTemplate = "%sDate"
 	@@timeTemplate = "%sTime"
+	@@calculateButtonIdentifier = 'Submit'
 
 
 	@@durationMap = { 
-		'30 minutes' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '12:30', 'AM']
+		'30 minutes' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '12:30', 'AM'],
+		'3 hours' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '03:00', 'AM'],
+		'5 hours' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '05:00', 'AM'],
+		'5 hours 1 minute' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '05:01', 'AM'],
+		'12 hours' => ['05/04/2010', '12:00', 'AM', '05/04/2010', '12:00', 'PM'],
+		'24 hours' => ['05/04/2010', '12:00', 'AM', '05/05/2010', '12:00', 'AM'],
+		'1 day 1 minute' => ['05/04/2010', '12:00', 'AM', '05/05/2010', '12:01', 'AM'],
+		'3 days' => ['05/04/2010', '12:00', 'AM', '05/07/2010', '12:00', 'AM'],
+		'1 week' => ['05/04/2010', '12:00', 'AM', '05/11/2010', '12:00', 'AM']
 	}
 	attr :page
 	
@@ -35,8 +44,15 @@ class ParkCalcPage
 	end
 	
 	def parking_costs
-		@page.button(:name, "Submit").click
-		cost_element = @page.tr(:text, /ESTIMATED PARKING COSTS/).td(:index, 1).span(:index, 0).text
-		return cost_element
+		calculate_parking_costs
+		get_parking_costs_from_page
+	end
+	
+	def calculate_parking_costs
+		@page.button(:name, @@calculateButtonIdentifier).click
+	end
+	
+	def get_parking_costs_from_page
+		@page.tr(:text, /ESTIMATED PARKING COSTS/).td(:index, 1).span(:index, 0).text
 	end
 end
